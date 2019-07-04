72148c93333d1af6f52985a8cb454407f94693c0
morfiy <edurnev@keaz.ru>
2019-07-04 14:17:38 +0300

    fix

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('autoplay.play.owl',[3000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('autoplay.stop.owl')
+})
