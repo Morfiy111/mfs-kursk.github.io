206271fe16996836a7ceb47d5a0a2a1e7305870b
morfiy <edurnev@keaz.ru>
2019-07-17 14:15:00 +0300

    favi

--- js/main.js
+++ js/main.js
@@ -77,6 +77,6 @@ jQuery(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-    $('.js-example-basic-single').select2();
-});
+//$(document).ready(function() {
+//    $('.js-example-basic-single').select2();
+//});
